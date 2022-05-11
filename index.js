c42fefe179d2a1bc266331dca58c7d1a03d32133
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2022-05-11 14:21:54 +0300

    Main loop

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const { INFO, ERROR, WARNING } = require('./logs');
+const config = require('./config');
+const { INFO, ERROR } = require('./logs');
 const { Migrations } = require('./migrations');
+const { Scraper } = require('./scraper');
 
 let migrations = new Migrations();
+let scraper = new Scraper(config.scraper.github_api, config.scraper.github_token);
+let stop = false;
 
+async function scrape() {
+    await scraper.Run();
+}
+
+const pause = (timeout) => new Promise(res => setTimeout(res, timeout * 1000));
 
 const mainLoop = async _ => {
     try {
         INFO('Run migrations');
         await migrations.run();
         INFO('Run migrations, done');
+
+        while (!stop) {
+            await scrape();
+
+            INFO(`Pause for 60 seconds`);
+            await pause(60);
+        }
+
     } catch (error) {
         ERROR(`[MainLoop] error :`);
         console.error(error);
