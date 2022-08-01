3556a787f71b8efcbafdf04740004b0f820c7de8
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2022-08-01 17:39:38 +0300

    Update views

--- migrations/00004_create-commits-view.sql
+++ migrations/00004_create-commits-view.sql
@@ -3,7 +3,7 @@ AS
     with
         commits_data as ( SELECT date_trunc('month', commit_date)::date AS commit_month, count(commit_hash) as commits FROM commits GROUP BY commit_month)
 
-    SELECT commits_data.commits, to_char(commit_month, 'Mon YY') as display_month FROM commits_data ORDER BY commit_month
+    SELECT commits_data.commits, commit_month, to_char(commits_data.commit_month, 'Mon YY') as display_month FROM commits_data ORDER BY commits_data.commit_month
 WITH DATA;
 
-CREATE UNIQUE INDEX IF NOT EXISTS idx_commits_view ON commits_view(display_month);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_commits_view ON commits_view(commit_month);
